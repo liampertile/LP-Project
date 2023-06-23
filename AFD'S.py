0026249c00bc34cb7b2dc624720b8c55fe4d3f0c
liampertile <tomaspertile340@gmail.com>
2023-06-23 06:51:14 -0300

    ahi si

--- AFD'S.py
+++ AFD'S.py
@@ -280,57 +280,25 @@ def afd_oprel(lexema):
 
 #AFD "opsuma"   
 def afd_opsuma(lexema):
-    estado_actual = 0
-    estados_aceptados = [6]
-    
+    estado_actual = 0 
     for caracter in lexema:
-        if estado_actual == 0 and caracter == 'o':
-            estado_actual = 1
-        elif estado_actual == 1 and caracter == 'p':
-            estado_actual = 2
-        elif estado_actual == 2 and caracter == 's':
-            estado_actual = 3
-        elif estado_actual == 3 and caracter == 'u':
-            estado_actual = 4
-        elif estado_actual == 4 and caracter == 'm':
-            estado_actual = 5
-        elif estado_actual == 5 and caracter == 'a':
-            estado_actual = 6
+        if estado_actual == 0 and caracter == '+':
+            estado_actual = 0
         else:
             estado_actual = -1
             return ESTADO_TRAMPA
-        
-    if estado_actual in estados_aceptados:
         return ESTADO_ACEPTADO
-    else:
-        return ESTADO_NO_ACEPTADO
 
 #AFD "opmult"   
-def afd_opmult(lexema):
-    estado_actual = 0
-    estados_aceptados = [6]
-    
+def afd_mult(lexema):
+    estado_actual = 0 
     for caracter in lexema:
-        if estado_actual == 0 and caracter == 'o':
-            estado_actual = 1
-        elif estado_actual == 1 and caracter == 'p':
-            estado_actual = 2
-        elif estado_actual == 2 and caracter == 'm':
-            estado_actual = 3
-        elif estado_actual == 3 and caracter == 'u':
-            estado_actual = 4
-        elif estado_actual == 4 and caracter == 'l':
-            estado_actual = 5
-        elif estado_actual == 5 and caracter == 't':
-            estado_actual = 6
+        if estado_actual == 0 and caracter == '*':
+            estado_actual = 0
         else:
             estado_actual = -1
             return ESTADO_TRAMPA
-        
-    if estado_actual in estados_aceptados:
         return ESTADO_ACEPTADO
-    else:
-        return ESTADO_NO_ACEPTADO
 
 #AFD "("
 def afd_parentesisIzq(lexema):
@@ -363,26 +331,7 @@ def afd_puntoycoma(lexema):
             estado_actual = -1
             return ESTADO_TRAMPA
         return ESTADO_ACEPTADO
-#AFD "*"
-def afd_mult(lexema):
-    estado_actual = 0 
-    for caracter in lexema:
-        if estado_actual == 0 and caracter == '*':
-            estado_actual = 0
-        else:
-            estado_actual = -1
-            return ESTADO_TRAMPA
-        return ESTADO_ACEPTADO
-#AFD "+"
-def afd_suma(lexema):
-    estado_actual = 0 
-    for caracter in lexema:
-        if estado_actual == 0 and caracter == '+':
-            estado_actual = 0
-        else:
-            estado_actual = -1
-            return ESTADO_TRAMPA
-        return ESTADO_ACEPTADO
+
 #AFD "<"
 def afd_menor(lexema):
     estado_actual = 0 
