48345ac7be0b7f3a065f2cbc738bd74a0262cacd
liampertile <tomaspertile340@gmail.com>
2023-06-24 22:33:46 -0300

    FINAL.

--- AFD'S.py
+++ AFD'S.py
@@ -280,25 +280,57 @@ def afd_oprel(lexema):
 
 #AFD "opsuma"   
 def afd_opsuma(lexema):
-    estado_actual = 0 
+    estado_actual = 0
+    estados_aceptados = [6]
+    
     for caracter in lexema:
-        if estado_actual == 0 and caracter == '+':
-            estado_actual = 0
+        if estado_actual == 0 and caracter == 'o':
+            estado_actual = 1
+        elif estado_actual == 1 and caracter == 'p':
+            estado_actual = 2
+        elif estado_actual == 2 and caracter == 's':
+            estado_actual = 3
+        elif estado_actual == 3 and caracter == 'u':
+            estado_actual = 4
+        elif estado_actual == 4 and caracter == 'm':
+            estado_actual = 5
+        elif estado_actual == 5 and caracter == 'a':
+            estado_actual = 6
         else:
             estado_actual = -1
             return ESTADO_TRAMPA
+        
+    if estado_actual in estados_aceptados:
         return ESTADO_ACEPTADO
+    else:
+        return ESTADO_NO_ACEPTADO
 
 #AFD "opmult"   
 def afd_opmult(lexema):
-    estado_actual = 0 
+    estado_actual = 0
+    estados_aceptados = [6]
+    
     for caracter in lexema:
-        if estado_actual == 0 and caracter == '*':
-            estado_actual = 0
+        if estado_actual == 0 and caracter == 'o':
+            estado_actual = 1
+        elif estado_actual == 1 and caracter == 'p':
+            estado_actual = 2
+        elif estado_actual == 2 and caracter == 'm':
+            estado_actual = 3
+        elif estado_actual == 3 and caracter == 'u':
+            estado_actual = 4
+        elif estado_actual == 4 and caracter == 'l':
+            estado_actual = 5
+        elif estado_actual == 5 and caracter == 't':
+            estado_actual = 6
         else:
             estado_actual = -1
             return ESTADO_TRAMPA
+        
+    if estado_actual in estados_aceptados:
         return ESTADO_ACEPTADO
+    else:
+        return ESTADO_NO_ACEPTADO
 
 #AFD "("
 def afd_parentesisIzq(lexema):
@@ -331,94 +363,3 @@ def afd_puntoycoma(lexema):
             estado_actual = -1
             return ESTADO_TRAMPA
         return ESTADO_ACEPTADO
-
-#AFD "<"
-def afd_menor(lexema):
-    estado_actual = 0 
-    for caracter in lexema:
-        if estado_actual == 0 and caracter == '<':
-            estado_actual = 0
-        else:
-            estado_actual = -1
-            return ESTADO_TRAMPA
-        return ESTADO_ACEPTADO
-#AFD ">"
-def afd_mayor(lexema):
-    estado_actual = 0 
-    for caracter in lexema:
-        if estado_actual == 0 and caracter == '>':
-            estado_actual = 0
-        else:
-            estado_actual = -1
-            return ESTADO_TRAMPA
-        return ESTADO_ACEPTADO
-#AFD "<="
-def afd_mayorigual(lexema):
-    estado_actual = 0
-    estados_aceptados = [2] 
-    for caracter in lexema:
-        if estado_actual == 0 and caracter == '<':
-            estado_actual = 1
-        elif estado_actual == 1 and caracter == '=':
-            estado_actual = 2
-        else:
-            estado_actual = -1
-            return ESTADO_TRAMPA
-        return ESTADO_ACEPTADO
-
-#AFD ">="
-def afd_mayorigual(lexema):
-    estado_actual = 0
-    estados_aceptados = [2] 
-    for caracter in lexema:
-        if estado_actual == 0 and caracter == '>':
-            estado_actual = 1
-        elif estado_actual == 1 and caracter == '=':
-            estado_actual = 2
-        else:
-            estado_actual = -1
-            return ESTADO_TRAMPA
-        return ESTADO_ACEPTADO
-
-#-------------------------------------------#
-TOKENS_POSIBLES = [("TOKEN ID",afd_id),("TOKEN NUM", afd_num),("TOKEN si", afd_si),("TOKEN sino", afd_sino),("TOKEN finsi", afd_finsi),("TOKEN repetir", afd_repetir)
-                   ,("TOKEN hasta", afd_hasta),("TOKEN equal", afd_equal),("TOKEN func", afd_func),("TOKEN finfunc", afd_finfunc),("TOKEN oprel", afd_oprel),("TOKEN opsuma", afd_opsuma),
-                   ("TOKEN opmult", afd_opmult),("TOKEN parentesisIzq", afd_parentesisIzq),("TOKEN parentesisDer", afd_parentesisDer),("TOKEN puntoycoma", afd_puntoycoma),]
-
-def lexer(codigofuente):
-    tokens = []
-    pos_actual = 0
-    while pos_actual < len(codigofuente):
-        while codigofuente[pos_actual].isspace():
-            pos_actual += 1
-        comienzo_lexema = pos_actual
-        posibles_tokens = []
-        posibles_tokens_con_un_caracter_mas = []
-        lexema = ""
-        todos_en_estado_trampa = False #CENTINELA
-        
-        while not todos_en_estado_trampa:
-            lexema = codigofuente[comienzo_lexema:pos_actual + 1]
-            posibles_tokens = posibles_tokens_con_un_caracter_mas
-            posibles_tokens_con_un_caracter_mas = []
-            
-            for (un_tipo_de_token , afd) in TOKENS_POSIBLES:
-                simulacion_afd = afd(lexema)
-                if simulacion_afd == ESTADO_ACEPTADO:
-                    posibles_tokens_con_un_caracter_mas.append(un_tipo_de_token)
-                    todos_en_estado_trampa = True
-                elif simulacion_afd == ESTADO_NO_ACEPTADO:
-                    todos_en_estado_trampa = False
-            pos_actual += 1
-        
-        if len(posibles_tokens) == 0:
-            print("ERROR:TOKEN NO VALIDO" + lexema)
-            
-        un_tipo_de_token = posibles_tokens [0]
-
-        token =(un_tipo_de_token,lexema)
-        tokens.append(token)
-        
-    return tokens
-
-
