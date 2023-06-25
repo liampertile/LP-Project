831474daaf5dbf16e9d473ce84328e8aff1925ef
liampertile <tomaspertile340@gmail.com>
2023-06-24 22:59:26 -0300

    23232

--- Lexer.py
+++ Lexer.py
@@ -9,14 +9,14 @@ TOKENS_POSIBLES = [("TOKEN ID", afd_id),("TOKEN NUM", afd_num),("TOKEN si", afd_
 
 tokens = []
 
-#lexema que terminen en estado trampa
+#strings que terminen en estado trampa
 
-def caeEnTrampa(lexema):
+def caeEnTrampa(string):
     todosCaen = True
     
     #Corro el lexema
     for (tipoToken,afd) in TOKENS_POSIBLES:
-        resultado = afd(lexema)
+        resultado = afd(string)
         #Si queda en No aceptado o aceptado
         if resultado == (ESTADO_ACEPTADO or ESTADO_NO_ACEPTADO):
             todosCaen = False
@@ -24,19 +24,59 @@ def caeEnTrampa(lexema):
 
     return caeEnTrampa
 
-#guardar el token del lexema en la lista "tokens"
+#guardar el token del strings en la lista "tokens"
 
 def guardarToken(lexema):
     global tokens
     
-    #Corremos el lexema en todos los automatas
+    #Corremos el string en todos los automatas
     
     for(tipoToken,afd) in TOKENS_POSIBLES:
-        resultado = afd(lexema)
+        resultado = afd(string)
     
         if resultado == (ESTADO_ACEPTADO):
-            tokens.append((tipoToken,lexema))
+            tokens.append((tipoToken,string))
         
         break
 
+def lexer(codigofuente):
+    codigofuente += ""
+    global tokens
+    tokens =[]
+    tokens_posi =[]
+    inicio = 0
+    final = 0
+    
+    while final <= len(codigofuente):
+        while codigofuente[inicio].isspace():
+            inicio +=1
+            final +=1
+            
+            if inicio == len(codigofuente):
+                break
+        lexema = codigofuente[inicio : final]
+        
+        #cuando no se generen estados trampas en los afd le seguimos metiendo caracteres
+        while not caeEnTrampa(lexema):
+            final +=1
+            lexema = codigofuente[inicio:final]
+        
+        final -=1
+        guardarToken(codigofuente[inicio:final])
+        
+        if inicio == final:
+            tokens_posi.append(codigofuente[inicio])
+            inicio += 1
+            final += 2
+        else:
+            inicio = final
+            final = inicio + 1
+        
+        if final == len(codigofuente):
+            break
+        
+print("TOKENS ERRONEOS:" + str(tokens_posi))
+return tokens
+
+        
 
