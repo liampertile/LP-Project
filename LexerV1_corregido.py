0baec18e3e8b046aab253e8ed1071b35ca6a2bb0
Tomás Liam Pertile <112728308+liampertile@users.noreply.github.com>
2023-08-21 18:12:04 -0300

    Update LexerV1_corregido.py

--- LexerV1_corregido.py
+++ LexerV1_corregido.py
@@ -1,7 +1,29 @@
 from AFD import *
 
-TOKENS_POSIBLES = [("TOKEN leer", afd_leer),  ("TOKEN NUM", afd_num), ("TOKEN si", afd_si), ("TOKEN sino", afd_sino), ("TOKEN finsi", afd_finsi), ("TOKEN repetir", afd_repetir), ("TOKEN hasta", afd_hasta), ("TOKEN equal", afd_equal), ("TOKEN func", afd_func),
-                   ("TOKEN finfunc", afd_finfunc), ("TOKEN menor", afd_menor),("TOKEN menorigual", afd_menorigual),("TOKEN mayor", afd_mayor),("TOKEN mayorigual", afd_mayorigual),("TOKEN igual", afd_igual),("TOKEN distinto", afd_distinto),("TOKEN suma", afd_suma), ("TOKEN resta", afd_resta), ("TOKEN mult", afd_mult),("TOKEN div", afd_div), ("TOKEN parentesisIzq", afd_parentesisIzq), ("TOKEN parentesisDer", afd_parentesisDer), ("TOKEN puntoycoma", afd_puntoycoma),("TOKEN ID", afd_id)] # Colocamos el TOKEN ID en último lugar para darle precedencia a palabras reservadas
+TOKENS_POSIBLES = [("TOKEN leer", afd_leer),
+                   ("TOKEN NUM", afd_num),
+                   ("TOKEN si", afd_si),
+                   ("TOKEN sino", afd_sino),
+                   ("TOKEN finsi", afd_finsi),
+                   ("TOKEN repetir", afd_repetir),
+                   ("TOKEN hasta", afd_hasta),
+                   ("TOKEN equal", afd_equal),
+                   ("TOKEN func", afd_func),
+                   ("TOKEN finfunc", afd_finfunc),
+                   ("TOKEN menor", afd_menor),
+                   ("TOKEN menorigual", afd_menorigual),
+                   ("TOKEN mayor", afd_mayor),
+                   ("TOKEN mayorigual", afd_mayorigual),
+                   ("TOKEN igual", afd_igual),
+                   ("TOKEN distinto", afd_distinto),
+                   ("TOKEN suma", afd_suma),
+                   ("TOKEN resta", afd_resta),
+                   ("TOKEN mult", afd_mult),
+                   ("TOKEN div", afd_div),
+                   ("TOKEN parentesisIzq", afd_parentesisIzq),
+                   ("TOKEN parentesisDer", afd_parentesisDer),
+                   ("TOKEN puntoycoma", afd_puntoycoma),
+                   ("TOKEN ID", afd_id)] # Colocamos el TOKEN ID en último lugar para darle precedencia a palabras reservadas
 
 tokens = []
 tokensErroneos = []
