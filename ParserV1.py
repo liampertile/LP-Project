8e730e89e0327f568b332218ea2630f4788a5850
Tomigonzalez5 <tomignlz5@gmail.com>
2023-10-21 20:30:15 -0300

    Completar Tabla de Producciones

--- ParserV1.py
+++ ParserV1.py
@@ -1,34 +1,58 @@
 from LexerV2 import *
 
-VN = ['Program','ListaSentencias','Sentencia','SentenciaSi','SentenciaRepetir','SentenciaAsig',
-  'SentenciaLeer','SentenciaMostrar','SentenciaFun','Proc','ListaPar','Expression','Expresion2','Factor','Termino']
+VN = ['Program', 'ListaSentencias', 'Sentencia', 'SentenciaSi', 'SentenciaRepetir', 'SentenciaAsig',
+      'SentenciaLeer', 'SentenciaMostrar', 'SentenciaFun', 'Proc', 'ListaPar', 'Expression', 'Expresion2', 'Factor', 'Termino']
 
 VT = TOKENS_POSIBLES
 
-P = { 'S': [['Token(','S','Token)','S'],
-            ['Token(','Token)','S'],
-            ['Token(','S','Token)'],
-            ['Token(','Token)']] }
+tabla = {
+    'Program': {
+        'si': ['ListaSentencias'],
+        'repetir': ['ListaSentencias'],
+        'id': ['ListaSentencias'],
+        'leer': ['ListaSentencias'],
+        'mostrar': ['ListaSentencias'],
+        'func': ['ListaSentencias']
+    },
+    'ListaSentencias': {
+        'si': ['Sentencia'],
+        'repetir': ['Sentencia'],
+        'id': ['Sentencia'],
+        'leer': ['Sentencia'],
+        'mostrar': ['Sentencia'],
+        'func': ['Sentencia']
+    },
+    'ListaSentenciasP': {
+
+    }
+}
+
+P = {'S': [['Token(', 'S', 'Token)', 'S'],
+           ['Token(', 'Token)', 'S'],
+           ['Token(', 'S', 'Token)'],
+           ['Token(', 'Token)']]}
+
 
 def lex(codigo_fuente):
-    lista_tokens=[]
+    lista_tokens = []
     for i in codigo_fuente:
-        if i=='(':
-            lista_tokens.append(('Token(','('))
-        elif i==')':
-            lista_tokens.append(('Token)',')'))
+        if i == '(':
+            lista_tokens.append(('Token(', '('))
+        elif i == ')':
+            lista_tokens.append(('Token)', ')'))
         else:
             return []
-    lista_tokens.append(('Eof','Eof'))
+    lista_tokens.append(('Eof', 'Eof'))
     return lista_tokens
+
 
 def desc_rec_proc(codigo_fuente):
     datos_locales = {
         'lista_tokens': codigo_fuente,
         'index': 0,
         'error': False,
-    }   
-          
+    }
+
     def pni(no_terminal):
         for cuerpo_produccion in P[no_terminal]:
             backtracking_index = datos_locales['index']
@@ -37,15 +61,15 @@ def desc_rec_proc(codigo_fuente):
                 datos_locales['index'] = backtracking_index
             else:
                 break
-    
+
     def procesar(cuerpo_produccion):
         for simbolo in cuerpo_produccion:
             caracter_actual = datos_locales['lista_tokens'][datos_locales['index']][0]
-            #lexema_actual = datos_locales['lista_tokens'][datos_locales['index']][1]
+            # lexema_actual = datos_locales['lista_tokens'][datos_locales['index']][1]
             datos_locales['error'] = False
             if simbolo in VT:
                 if simbolo == caracter_actual:
-                    datos_locales['index'] += 1                        
+                    datos_locales['index'] += 1
                 else:
                     datos_locales['error'] = True
                     break
@@ -53,8 +77,7 @@ def desc_rec_proc(codigo_fuente):
                 pni(simbolo)
                 if datos_locales['error']:
                     break
-                
-    
+
     def principal():
         pni('S')
         caracter_actual = datos_locales['lista_tokens'][datos_locales['index']][0]
@@ -63,7 +86,8 @@ def desc_rec_proc(codigo_fuente):
             return False
         print('La cadena pertenece al lenguaje')
         return True
-    
+
     return principal()
+
 
 print(desc_rec_proc(lex('(())')))
