30d7087fe3ca67dc238e92d4a67a02da0a316fa5
liampertile <tomaspertile340@gmail.com>
2023-11-06 13:31:38 -0300

    listo

--- PruebasParser.py
+++ PruebasParser.py
@@ -7,6 +7,4 @@ parser(traduccionParser(lexer("aux equal 5")))
 parser(traduccionParser(lexer("func aux (aux2) leer aux3 finfunc")))
 parser(traduccionParser(lexer("aux equal 9")))
 parser(traduccionParser(lexer("mostrar 5")))
-
-# No funciona, debido a que cuando calculamos los SD(TerminoP), dentro de estos no está "TOKEN NUM".
 parser(traduccionParser(lexer("repetir mostrar 3 hasta aux3")))
